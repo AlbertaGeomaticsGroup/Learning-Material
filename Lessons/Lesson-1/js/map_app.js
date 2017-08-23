b34eee9d76a03c687dbc90891d67ead44bb0909f
James Banting <jrbanting@gmail.com>
2017-08-22 20:06:44 -0600

    Added Lesson 2 Code and updated attribution on Lesson 1

--- Lessons/Lesson-1/js/map_app.js
+++ Lessons/Lesson-1/js/map_app.js
@@ -1,7 +1,14 @@
-var map = L.map('map').setView([54, -114], 6);
+var map = L.map('map', {
+    center: [54, -114],
+    zoom: 6,
+    attributionControl: false // We define our own attribution
+});
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: ' &copy;  <a href="http://openstreetmap.org">OpenStreetMap</a> Contributors',
     id: 'mapbox.streets'
 }).addTo(map);
+
+// Set Custom Attribution
+L.control.attribution().addAttribution("<a target='_blank' href='https://www.albertageomaticsgroup.ca/'>Alberta Geomatics Group</a>").addTo(map)
